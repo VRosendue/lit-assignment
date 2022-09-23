c3164165fc049b6f2389e385e33f653808c63db3
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2022-09-23 17:09:23 +0200

    Translations signs are working now

--- src/components/Translator/Translations.jsx
+++ src/components/Translator/Translations.jsx
@@ -1,7 +1,7 @@
 const Translations = ({letter}) => {
     return (
         <>
-            {letter !== " " ? <img src={`/img${letter}.png`} alt={letter} width="55"/> : '\xa0\xa0\xa0\xa0\xa0\xa0\xa0'}
+            {letter !== " " ? <img src={`/img/${letter}.png`} alt={letter} width="55"/> : '\xa0\xa0\xa0\xa0\xa0\xa0\xa0'}
         </>
     );
 }
