2037bcb116be36c064115b6d445e6ca96b1493f5
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-09-24 19:58:11 +0200

    Added CSS

--- src/Profile/ProfileHeader.jsx
+++ src/Profile/ProfileHeader.jsx
@@ -1,8 +1,21 @@
+import "../Profile/ProfileHeader.css";
+
 const ProfileHeader = ({ username }) => {
     return (
-       <header>
-        <h4> Hello, welcome back {username}</h4>
-       </header>
+        <div className="profileHeader">
+        <div className="profileHeaderPicture">
+            <img className="logo3" src="Splash.svg" alt=""></img>
+            <img className="logo2" src="Logo.png" alt=""></img>
+            
+        </div>
+   
+        <div className="headerFont">
+        <h1> Lost in Translation </h1>
+    
+    <h4> Hello, welcome back {username}</h4>
+   
+   </div>
+   </div>
     )
 }
 export default ProfileHeader
